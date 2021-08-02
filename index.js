df4122e61534b23cf68254a3fff6fa361eb8f857
Heather <heathermhargreaves@users.noreply.github.com>
2021-08-02 16:09:17 -0400

    adding endpoints to handle webhook events

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken); 
+const twilio = require('twilio')(accountSid, authToken); 
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const Analytics = require('analytics-node');
 const analytics = new Analytics(process.env.SEGMENT_KEY);
 const ABTesting = require('ab-testing');
 
+
 // user profile
 let user = {
-    userId: 'ENTER USER ID',
-    name: 'NAME',
-    number: 'NUMBER',
-    email: 'EMAIL'
+    userId: '12',
+    name: 'Stacy',
+    number: process.env.PHONE_NUMBER,
+    email: process.env.EMAIL
 }
 
 // Twilio and Sendgrid info to be used for AB testing logic later
 let twilio_info = {
-    from: 'YOUR TWILIO NUMBER',
+    from: '+15102966273',
     body: `Reminder for ${user.name} your appointment is coming up tomorrow`,
     twiml: `<Response><Say>Reminder for ${user.name} your appointment is coming up tomorrow</Say></Response>`
 }
 
 const email_msg = {
     to: user.email, 
-    from: 'YOUR SENDGRID EMAIL', 
+    from: 'help@meeting-icebreaker.com', 
     subject: 'Your Appointment is coming up',
     text: 'Your Appointment is coming up',
     html: `<strong>Your appointment is tomorrow, ${user.name}, let us know if you need to make a change.</strong>`,
@@ -43,14 +48,14 @@ analytics.identify({
     }
 });
 
-
-var channelTest = ABTesting.createTest('channelTest', [{ name: 'SMS' }, { name: 'CALL' }, { name: 'EMAIL' }, { name: 'WHATSAPP' }]);
+// Creating AB test name and setting variation names
+var channelTest = ABTesting.createTest('channelTest', [{ name: 'SMS' }, { name: 'CALL' }, { name: 'EMAIL' } ]);
 
 var channelTestVariation = channelTest.getGroup(user.userId);
 
 console.log(channelTest.getName(), channelTestVariation)
 
-// segment call to track experiment info
+// Segment call to track experiment info
 analytics.track({
     userId: user.userId,
     event: 'Experiment Viewed',
@@ -64,7 +69,7 @@ analytics.track({
 channelTest.test(channelTestVariation, [
 	function () {
         console.log("sms")
-		client.messages
+		twilio.messages
         .create({
             body: twilio_info.body,
             from: twilio_info.from,
@@ -75,11 +80,15 @@ channelTest.test(channelTestVariation, [
 	function () {
 		// CALL code
         console.log('call')
-        client.calls
+        twilio.calls
         .create({
             twiml: twilio_info.twiml,
             from: twilio_info.from,
-            to: user.number
+            to: user.number,
+            method: 'GET',
+            statusCallback: `${process.env.NGROK_URL}/call`,
+            statusCallbackEvent: ['answered'],
+            statusCallbackMethod: 'POST'
         })
         .then(call => console.log(call.sid));
 	},
@@ -90,25 +99,55 @@ channelTest.test(channelTestVariation, [
         .send(email_msg)
         .then((response) => {
           console.log(response[0].statusCode)
-          console.log(response[0].headers)
         })
         .catch((error) => {
           console.error(error)
         })
-	},
-	function () {
-		// WHATSAPP code
-        console.log('whatsapp')
-        client.messages
-        .create({
-            body: twilio_info.body,
-            from: 'whatsapp:+14155238886',
-            to: `whatsapp:${user.number}`
-        })
-        .then(message => console.log(message.sid));
-	}
-], this);
+ 	}
+ ], this);
 
+// endpoints for webhooks to capture responses and answers
+app.post('/sms', (req, res) => {
+    
+    analytics.track({
+        userId: user.userId,
+        event: 'Responded to SMS',
+        properties: {
+          user_response: req.body.Body,
+          experiment_name: channelTest.getName(),
+          variation_name: channelTestVariation
+        }
+    });
+
+    res.sendStatus(200);
+
+  });
+
+  app.post('/call', (req, res) => {
+    
+    analytics.track({
+        userId: user.userId,
+        event: 'Answered Call',
+        properties: {
+          experiment_name: channelTest.getName(),
+          variation_name: channelTestVariation
+        }
+    });
+  
+  });
+
+  app.post('/email', (req, res) => {
+    
+    analytics.track({
+        userId: user.userId,
+        event: 'Opened Email',
+        properties: {
+          experiment_name: channelTest.getName(),
+          variation_name: channelTestVariation
+        }
+    });
+    
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
